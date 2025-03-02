fd85fc5734d5ee5b11b4f6b90c34033209aaa46d
loverelyas <129636393+loverelyas@users.noreply.github.com>
2025-03-02 04:42:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="spider-api",
-    version="1.6", 
+    version="1.7", 
     description="my lib is power",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
